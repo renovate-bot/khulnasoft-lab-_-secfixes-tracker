c41e645e6d05b010aca53dc9245cd79ab6dfdcba
NxPKG <116948796+NxPKG@users.noreply.github.com>
2024-06-06 12:17:46 +0600

    Create secfixes-tracker.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

---
+++ secfixes-tracker.py
@@ -0,0 +1,9 @@
+from secfixes_tracker import create_app
+
+app = create_app()
+
+from secfixes_tracker import models, importers, views
+
+models.register(app)
+importers.register(app)
+views.register(app)
